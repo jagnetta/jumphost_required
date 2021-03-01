6c19e0e5e0f4b5d2c50d45acc22ec00177608ef1
Jim Agnetta <jagnetta@fuze.com>
2021-03-01 17:09:05 -0500

    added comments and pretzel logic

--- jumphost_required.sh
+++ jumphost_required.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-# 
+# we're testing to see if we need to use the jumphost.  we don't need the jumphost when the target IP address starts with 10.
+# this script is called by ssh config, so that script is checking to see if it needs the jumphost.
+# if this script exits successfully, then it means that input was provided, and that the ip address did not start with 10
+# in the second case we're forcing the "false" value "1" if the "starts with a 10" condition is met
+# if the 10 is found, we exit with 1 and that tells ssh we didn't need the jump
+# if the value turns out to not be a 10, then this script exits successfully, and ssh invokes the jump condiion. 
 #
 # If there were no arguments provided, exit.  You really don't want to wait for nslookup to timeout over nothing, do you?
-#
 if [ -z $1  ]; then
 	exit 1
-# do a lookup on the input and unless the IP starts with a 10 then fail
+# do a lookup on the input and if the IP address starts with a 10, then fail; otherwise, exit successfully, and let SSH use the jumphost 
 #
 elif [[ `nslookup $1 | tail -2 | cut -d ' ' -f 2 | cut -d '.' -f 1` == 10 ]]; then
 	exit 1
