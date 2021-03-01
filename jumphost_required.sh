f47d3ff2f40dbaff5fbac020600b73fdee02b773
Jim Agnetta <jagnetta@fuze.com>
2021-03-01 15:50:32 -0500

    changed branch to make sure a good hostname was entered

--- jumphost_required.sh
+++ jumphost_required.sh
@@ -8,8 +8,8 @@ if [ -z $1  ]; then
 else
 # do a lookup on the input and if the IP starts with a 10 then fail
 #
-	result=`nslookup $1 | tail -2 | cut -d ' ' -f 2 | cut -d '.' -f 1`
-	if [ $result == 10 ]; then
+	# result=`nslookup $1 | tail -2 | cut -d ' ' -f 2 | cut -d '.' -f 1`
+	if [[ `nslookup $1 | tail -2 | cut -d ' ' -f 2 | cut -d '.' -f 1` == 10 || -n `nslookup $1 | grep \*` ]]; then
        		exit 1
 	fi
 fi
