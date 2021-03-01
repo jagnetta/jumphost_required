f1cf0f2f40e524ab59bbbdcdc262d7979516a90f
Jim Agnetta <jagnetta@fuze.com>
2021-03-01 17:00:56 -0500

    more editing

--- jumphost_required.sh
+++ jumphost_required.sh
@@ -7,6 +7,6 @@ if [ -z $1  ]; then
 	exit 1
 # do a lookup on the input and unless the IP starts with a 10 then fail
 #
-elif [[ `nslookup $1 | tail -2 | cut -d ' ' -f 2 | cut -d '.' -f 1` != 10 ]]; then
+elif [[ `nslookup $1 | tail -2 | cut -d ' ' -f 2 | cut -d '.' -f 1` == 10 ]]; then
 	exit 1
 fi
