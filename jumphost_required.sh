fe6e5530f7f765f2e1ed1dd673d476b01adcb96b
Jim Agnetta <jagnetta@fuze.com>
2021-03-01 16:45:38 -0500

    changed logic AGAIN

--- jumphost_required.sh
+++ jumphost_required.sh
@@ -5,10 +5,8 @@
 #
 if [ -z $1  ]; then
 	exit 1
-else
 # do a lookup on the input and unless the IP starts with a 10 then fail
 #
-	if [[ `nslookup $1 | tail -2 | cut -d ' ' -f 2 | cut -d '.' -f 1` == 10 ]]; then
-       		exit 1
-	fi
+elif [[ `nslookup $1 | tail -2 | cut -d ' ' -f 2 | cut -d '.' -f 1` != 10 ]]; then
+	exit 1
 fi
