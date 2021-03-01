13cc3515e5f4b60538ac98ea9f8e2f7fbe8d9c9c
Jim Agnetta <jagnetta@fuze.com>
2021-03-01 16:16:18 -0500

    changed logic again, looking for a "perfect 10"

--- jumphost_required.sh
+++ jumphost_required.sh
@@ -9,7 +9,7 @@ else
 # do a lookup on the input and if the IP starts with a 10 then fail
 #
 	# result=`nslookup $1 | tail -2 | cut -d ' ' -f 2 | cut -d '.' -f 1`
-	if [[ `nslookup $1 | tail -2 | cut -d ' ' -f 2 | cut -d '.' -f 1` == 10 || -n `nslookup $1 | grep \*` ]]; then
+	if [[ `nslookup $1 | tail -2 | cut -d ' ' -f 2 | cut -d '.' -f 1` != 10 ]]; then
        		exit 1
 	fi
 fi
