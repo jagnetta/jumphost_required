75004c9a537257c0a450759a4182f778dc9ab18b
Jim Agnetta <jagnetta@fuze.com>
2021-03-01 16:19:43 -0500

    feeling cute one more edit

--- jumphost_required.sh
+++ jumphost_required.sh
@@ -6,9 +6,8 @@
 if [ -z $1  ]; then
 	exit 1
 else
-# do a lookup on the input and if the IP starts with a 10 then fail
+# do a lookup on the input and unless the IP starts with a 10 then fail
 #
-	# result=`nslookup $1 | tail -2 | cut -d ' ' -f 2 | cut -d '.' -f 1`
 	if [[ `nslookup $1 | tail -2 | cut -d ' ' -f 2 | cut -d '.' -f 1` != 10 ]]; then
        		exit 1
 	fi
