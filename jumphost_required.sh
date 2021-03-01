a9455d0b8e37a4852421eb48795ac6e292f1493b
Jim Agnetta <jagnetta@fuze.com>
2021-03-01 16:30:30 -0500

    forgot to change the operator....sigh.

--- jumphost_required.sh
+++ jumphost_required.sh
@@ -8,7 +8,7 @@ if [ -z $1  ]; then
 else
 # do a lookup on the input and unless the IP starts with a 10 then fail
 #
-	if [[ `nslookup $1 | tail -2 | cut -d ' ' -f 2 | cut -d '.' -f 1` != 10 ]]; then
+	if [[ `nslookup $1 | tail -2 | cut -d ' ' -f 2 | cut -d '.' -f 1` == 10 ]]; then
        		exit 1
 	fi
 fi
