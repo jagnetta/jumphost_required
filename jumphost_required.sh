652c396c122fa579d6f17368508721ea44c734ab
Jim Agnetta <jagnetta@fuze.com>
2021-03-01 13:43:01 -0500

    more cleaning of jumphost_required.sh

--- jumphost_required.sh
+++ jumphost_required.sh
@@ -3,15 +3,13 @@
 #
 # If there were no arguments provided, exit.
 #
-if [ -z $1  ]
-	then
+if [ -z $1  ]; then
 	exit 1
 else
 # do a lookup on the input and if the IP starts with a 10 then fail
 #
-result=`nslookup $1 | tail -2 | cut -d ' ' -f 2 | cut -d '.' -f 1`
-if [ $result == 10 ]
-	then
-       	exit 1
+	result=`nslookup $1 | tail -2 | cut -d ' ' -f 2 | cut -d '.' -f 1`
+	if [ $result == 10 ]; then
+       		exit 1
 	fi
 fi
