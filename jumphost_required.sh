1fc5615bce73bf883a5f8f94ca954e63ad1cefae
Jim Agnetta <jagnetta@fuze.com>
2021-03-01 13:20:53 -0500

    changed jumphost_required.sh file and cleaned up the if statements in there.

--- jumphost_required.sh
+++ jumphost_required.sh
@@ -2,26 +2,22 @@
 # 
 # Check whether the user had supplied -h or --help.  If yes, then display usage example.
 #
-	if [[ $1 == "--help" || $1 == "-h" ]]
-		then
-			echo -e "\nThis script will attempt to determine if a jumphost is needed to reach the target server.\nUsage: ./jumphost_needed.sh [hostname]\n"
-			exit 1
-	fi
+	#if [[ $1 == "--help" || $1 == "-h" ]]
+		#then
+			#echo -e "\nThis script will attempt to determine if a jumphost is needed to reach the target server.\nUsage: ./jumphost_needed.sh [hostname]\n"
+			#exit 1
+	#fi
 #
 # If there were no arguments provided, and they didn't ask for help, prompt for the input and continue.
 #
-	if [ $# -lt 1 ]
+	if [ -z $1  ]
 		then
-			echo -e -n "\nEnter the servername please (e.g., "ukp-g1" or "ukp-g1.tpn.thinkingphones.net").\n\tServer Name:  "
-			read -r hostname
-		else
-			hostname=$1
-	fi
-#
-#
-result=`nslookup $hostname | tail -2 | cut -d ' ' -f 2 | cut -d '.' -f 1`
-
-if [ $result == 10 ]
-then
 		exit 1
-fi
+	else
+	result=`nslookup $1 | tail -2 | cut -d ' ' -f 2 | cut -d '.' -f 1`
+	if [ $result == 10 ]
+		then
+               		 exit 1
+		fi
+
+	fi
